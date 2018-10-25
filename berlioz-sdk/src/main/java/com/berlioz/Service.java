b3f6bde435faef38d4d4ea624e2a13dcdbc73543
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-10-24 23:15:10 -0700

    Service interface

--- berlioz-sdk/src/main/java/com/berlioz/Service.java
+++ berlioz-sdk/src/main/java/com/berlioz/Service.java
@@ -1,15 +1,17 @@
 package com.berlioz;
 
 import com.berlioz.agent.Client;
+import com.berlioz.comm.Endpoint;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Map;
 
 public class Service {
     private static Logger logger = LogManager.getLogger(Service.class);
 
     String _id;
     String _endpoint;
-
 
     Service(String id)
     {
@@ -22,7 +24,28 @@ public class Service {
         this._endpoint = endpoint;
     }
 
-    public void all() {
-        logger.info("Service {} :: ALL", this._id);
+    public void monitorAll(Registry.Callback<Map<String, Endpoint>> cb)
+    {
+
+    }
+
+    public void monitorFirst(Registry.Callback<Endpoint> cb)
+    {
+
+    }
+
+    public Map<String, Endpoint> all()
+    {
+        return null;
+    }
+
+    public Endpoint first()
+    {
+        return null;
+    }
+
+    public Endpoint random()
+    {
+        return null;
     }
 }
