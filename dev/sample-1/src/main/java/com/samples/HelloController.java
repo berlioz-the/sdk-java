9c06bfc80a9310099f87fb02df2b40bc62a85a23
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-11-02 12:16:28 -0700

    basic tracing

--- dev/sample-1/src/main/java/com/samples/HelloController.java
+++ dev/sample-1/src/main/java/com/samples/HelloController.java
@@ -2,56 +2,51 @@ package com.samples;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
-//import zipkin.Span;
-//import zipkin.reporter.AsyncReporter;
-//import zipkin.reporter.Encoding;
-//import zipkin.reporter.Reporter;
-//import zipkin.reporter.Sender;
-//import zipkin.reporter.okhttp3.OkHttpSender;
-//import zipkin2.Endpoint;
 
-import java.util.concurrent.TimeUnit;
+import zipkin2.Endpoint;
+import zipkin2.Span;
+import zipkin2.codec.Encoding;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.urlconnection.URLConnectionSender;
 
 @RestController
 public class HelloController {
 
+    URLConnectionSender sender = URLConnectionSender.newBuilder()
+            .endpoint("http://localhost:40009/api/v2/spans")
+            .encoding(Encoding.JSON)
+//            .connectTimeout(config.getConnectTimeout())
+//            .compressionEnabled(config.isCompressionEnabled())
+//            .readTimeout(config.getReadTimeout())
+            .build();
+
+    Reporter<Span> reporter = AsyncReporter.create(sender);
+    //Reporter.CONSOLE; //
+
     @RequestMapping("/")
     public String index() {
-//        Reporter.CONSOLE.report(span);
-//        AsyncReporter.create(URLConnectionSender.create)
-//        Sender sender = OkHttpSender.builder()
-//                .endpoint("http://localhost:40008" + "/api/v1/spans")
-//                .encoding(Encoding.JSON)
-//                .build();
 
-//        AsyncReporter.
+        Endpoint localEndpoint = Endpoint.newBuilder().serviceName("mama").ip("192.168.0.1").build();
 
-//        AsyncReporter.create(URLConnectionSender.create("http://localhost:9411/api/v2/spans"));
+        long start = 1000 * System.currentTimeMillis();
 
-//        AsyncReporter<Span> reporter = AsyncReporter.builder(sender)
-//                .closeTimeout(1, TimeUnit.MILLISECONDS)
-//                .build();
-//
-//        Endpoint localEndpoint = Endpoint.newBuilder().serviceName("tweetie").ip("192.168.0.1").build();
-//
-//        span = Span.newBuilder()
-//                .traceId("d3d200866a77cc59")
-//                .id("d3d200866a77cc59")
-//                .name("targz")
-//                .localEndpoint(localEndpoint)
-//                .timestamp(epochMicros())
-//                .duration(durationInMicros)
-//                .putTag("compression.level", "9");
-//
-//
-//        return new Tuple<>(Tracing.newBuilder()
-//                .localServiceName(serviceName)
-//                .sampler(Sampler.ALWAYS_SAMPLE)
-//                .traceId128Bit(true)
-//                .reporter(reporter)
-//                .build(), () -> reporter.flush());
+        Span span = Span.newBuilder()
+                .traceId("d3d200866a77cc63")
+                .id("d3d200866a77cc63")
+                .name("targz")
+                .localEndpoint(localEndpoint)
+                .timestamp(start)
+                .duration(10000)
+                .addAnnotation(start, "RECV")
+                .addAnnotation(start + 1000, "SENT")
+                .putTag("census.status_code", "OK")
+                .build();
 
-        return "Hello World!";
+
+        reporter.report(span);
+
+        return "Hello World!  " + start;
     }
 
 }
