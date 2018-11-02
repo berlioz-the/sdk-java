631677d128f732b790cfe1f3663303fb354decfc
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-11-02 13:20:35 -0700

    added basic interceptor

---
+++ dev/sample-1/src/main/java/com/samples/BerliozHandler.java
@@ -0,0 +1,29 @@
+package com.samples;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BerliozHandler extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.printf("BERLIOZ:: preHandle\n");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        System.out.printf("BERLIOZ:: postHandle\n");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+        System.out.printf("BERLIOZ:: afterCompletion\n");
+    }
+
+}
