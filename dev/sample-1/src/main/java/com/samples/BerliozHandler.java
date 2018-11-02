bff9c0a6f17d8700da87952b1e0d70f3096ecde5
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-11-02 14:28:57 -0700

    added zipkin tags

--- dev/sample-1/src/main/java/com/samples/BerliozHandler.java
+++ dev/sample-1/src/main/java/com/samples/BerliozHandler.java
@@ -1,18 +1,47 @@
 package com.samples;
 
+import brave.Tracer;
+import brave.Tracing;
 import org.springframework.lang.Nullable;
-import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import zipkin2.Span;
+import zipkin2.codec.Encoding;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Reporter;
+import zipkin2.reporter.urlconnection.URLConnectionSender;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class BerliozHandler extends HandlerInterceptorAdapter {
+
+
+    URLConnectionSender sender = URLConnectionSender.newBuilder()
+            .endpoint("http://localhost:40009/api/v2/spans")
+            .encoding(Encoding.JSON)
+            .build();
+
+    Reporter<Span> reporter = AsyncReporter.create(sender);
+//    Reporter.CONSOLE; //
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.printf("BERLIOZ:: preHandle\n");
+
+        Tracing tracing = Tracing.newBuilder()
+                .localServiceName("another-service")
+                .spanReporter(reporter)
+                .build();
+        Tracer tracer = tracing.tracer();
+        brave.Span span = tracer.newTrace()
+                .name(request.getMethod())
+                .tag("http.url", request.getRequestURL().toString())
+                .annotate("sr");
+
+        HttpSession session = request.getSession();
+        session.setAttribute("BERLIOZ_SPAN", span);
         return true;
     }
 
@@ -24,6 +53,11 @@ public class BerliozHandler extends HandlerInterceptorAdapter {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
         System.out.printf("BERLIOZ:: afterCompletion\n");
+
+        HttpSession session = request.getSession();
+        brave.Span span = (brave.Span)session.getAttribute("BERLIOZ_SPAN");
+        span.tag("http.status_code", String.valueOf(response.getStatus()));
+        span.annotate("ss");
     }
 
 }
