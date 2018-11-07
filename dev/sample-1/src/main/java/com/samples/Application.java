38b1630a08b6f0a7c4fef420869684b782cb9a63
Ruben Hakopian <ruben.hakopian@gmail.com>
2018-11-07 15:09:05 -0800

    minor changes.

--- dev/sample-1/src/main/java/com/samples/Application.java
+++ dev/sample-1/src/main/java/com/samples/Application.java
@@ -3,7 +3,7 @@ package com.samples;
 import com.berlioz.Berlioz;
 
 import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
--- dev/sample-1/src/main/java/com/samples/WebConfig.java
+++ dev/sample-1/src/main/java/com/samples/WebConfig.java
@@ -4,7 +4,6 @@ import com.berlioz.mysql.BerliozDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
