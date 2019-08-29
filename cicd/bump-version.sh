277a17dd01a97df7a46601c0dad7588d2cc75e7c
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-08-29 16:38:34 -0700

    added cicd bump version script

--- cicd/bump-version.sh
+++ cicd/bump-version.sh
@@ -1,5 +1,5 @@
 MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 MY_DIR="$(dirname $MY_PATH)"
 source ${MY_DIR}/build-version.sh
-export BERLIOZ_SDK_VERSION="1.1.${BERLIOZ_SDK_BUILD_VERSION}-SNAPSHOT"
-echo "BERLIOZ_SDK_VERSION=${BERLIOZ_SDK_VERSION}"
+echo "export BERLIOZ_SDK_BUILD_VERSION=$((BERLIOZ_SDK_BUILD_VERSION+1))" > ${MY_DIR}/build-version.sh
+source ${MY_DIR}/version.sh
