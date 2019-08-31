c9033f55827534631849bffee96f0dc7ab295d71
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-08-31 22:32:48 +0100

    added deprecation message
    Modified Areas:
    publish.sh
    
    Files Changed:
    M  publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "THIS PROCEDURE IS DEPRECATED. FOR RELEASE WE USE CI/CD PIPELINE."
+read
+
 MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 MY_DIR="$(dirname $MY_PATH)"
 echo $MY_DIR
